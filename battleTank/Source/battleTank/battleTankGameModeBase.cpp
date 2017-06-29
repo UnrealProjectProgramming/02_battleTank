679154bbf78305026d02154b3b139edde78947a1
Mazen Morgan <morgan.mazen42@gmail.com>
2017-06-29 18:24:30 +0200

    Landscape

---
+++ battleTank/Source/battleTank/battleTankGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "battleTank.h"
+#include "battleTankGameModeBase.h"
+
+
+
+
---
+++ battleTank/Source/battleTank/battleTankGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "battleTankGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API AbattleTankGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
