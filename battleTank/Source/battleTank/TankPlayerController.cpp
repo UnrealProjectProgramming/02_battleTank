6a5469b508bc02ed0d1dc59eb63b56fd33f3fd10
Mazen Morgan <morgan.mazen42@gmail.com>
2017-07-01 21:55:47 +0200

    BeginPlayer Overriding

--- battleTank/Source/battleTank/TankPlayerController.cpp
+++ battleTank/Source/battleTank/TankPlayerController.cpp
@@ -3,7 +3,18 @@
 #include "battleTank.h"
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("ATankplayer Controller Begin Player ! "))
 
+	auto Tank = GetControlledTank();
+	if (Tank) 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s "), *GetPawn()->GetName())
+	}
+	
+}
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
--- battleTank/Source/battleTank/TankPlayerController.h
+++ battleTank/Source/battleTank/TankPlayerController.h
@@ -18,4 +18,5 @@ public:
 	
 	ATank* GetControlledTank() const;
 		
+	virtual void BeginPlay() override;
 };
