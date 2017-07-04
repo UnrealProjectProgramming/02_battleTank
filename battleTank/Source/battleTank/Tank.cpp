9c8d1b36c94fa1c49715d379d40ff1600b002015
Mazen Morgan <morgan.mazen42@gmail.com>
2017-07-05 00:57:16 +0200

    Creating Aiming Component from cpp script

--- battleTank/Source/battleTank/Tank.cpp
+++ battleTank/Source/battleTank/Tank.cpp
@@ -10,6 +10,7 @@ ATank::ATank()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
 }
 
 // Called when the game starts or when spawned
@@ -35,7 +36,6 @@ void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 
 void ATank::AimAt(FVector HitLocation)
 {
-	FString OurTankName = GetName();
-	UE_LOG(LogTemp, Warning, TEXT("%s  aiming at  %s"), *OurTankName, *HitLocation.ToString());
+	TankAimingComponent->AimAt(HitLocation);
 }
 
--- battleTank/Source/battleTank/Tank.h
+++ battleTank/Source/battleTank/Tank.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "TankAimingComponent.h"
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h"
 
@@ -17,7 +18,7 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
+	UTankAimingComponent* TankAimingComponent = nullptr;
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
